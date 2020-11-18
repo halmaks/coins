d99f264d843253dde3792abf3b2fc825b4cb7262
halmaks <halatsiankou@gmail.com>
2020-11-18 13:25:42 +0300

    added authorization

---
+++ src/main/java/com/halmaks/coins/config/AuthorizationServerConfig.java
@@ -0,0 +1,18 @@
+package com.halmaks.coins.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+// https://dzone.com/articles/spring-boot-oauth2-getting-the-authorization-code
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+//	@Override
+//	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//		clients.inMemory().withClient("javainuse").secret("secret").authorizedGrantTypes
+//			.scopes("read").authorities("CLIENT");
+//		}
+}
---
+++ src/main/java/com/halmaks/coins/config/CustomerSecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.halmaks.coins.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+// https://dzone.com/articles/spring-boot-oauth2-getting-the-authorization-code
+@Configuration
+@EnableWebSecurity
+public class CustomerSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/api/v1/customer")
+			.hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
+			.permitAll().and().logout().permitAll();
+
+		http.csrf().disable();
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+	    authenticationMgr.inMemoryAuthentication().withUser("admin").password("admin")
+		    .authorities("ROLE_ADMIN");
+	}
+}
+			
+

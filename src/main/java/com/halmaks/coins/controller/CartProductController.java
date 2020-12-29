e5d1e2e387d5a3621f2faf5e6649022cc2d0263f
halmaks <halatsiankou@gmail.com>
2020-12-30 01:53:23 +0300

    added cross domain access

--- src/main/java/com/halmaks/coins/controller/CartProductController.java
+++ src/main/java/com/halmaks/coins/controller/CartProductController.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1")
 public class CartProductController {
--- src/main/java/com/halmaks/coins/controller/CustomerController.java
+++ src/main/java/com/halmaks/coins/controller/CustomerController.java
@@ -9,19 +9,13 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.halmaks.coins.exception.ResourceNotFoundException;
 import com.halmaks.coins.model.Customer;
 import com.halmaks.coins.repository.CustomerRepository;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/authenticated")
 public class CustomerController {
